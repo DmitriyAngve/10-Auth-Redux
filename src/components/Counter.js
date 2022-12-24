42936786b58ee8d7a36a650cfd2bbfbc1a1771a1
Dmitriy Angve <kirirch129@gmail.com>
2022-12-24 12:28:55 +0300

    #

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,9 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -13,3 +16,16 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// ~~ USING REDUX DATA IN REACT COMPONENTS ~~
+// Start by outputting the current counter. For this we need to get access to our Redux store and to the data in there
+// STEP: 1
+// 1.1 Import Reack Hook from a React-Redux library. A custom hook made by the React-Redux team: "import { useSelector } from "react-redux"", that allows us to then automatically select a part of our state managed by the store.
+// For functional Component "import { useSelector, connect } from "react-redux"". "connect" function use as a wrapper around our class Component to connect that class Component to the "store".
+// 1.2 In the "Counter" Component we can get access to the data managed in our store by using use selector. "useSelector()"
+// 1.3 After calling this we need to pass a function to "useSelector", a function which will be executed by React-Redux, a function which then basically determines which piece of data we wanna extract from our store.
+// 1.3.1 Pass a function of which we'll receive the state managed by Redux and then we return the part of the state which we wanna extract " useSelector((state) => state.counter)" -> this function will be executed for us by React-Redux. It will then pass the Redux state, so the manage the data into this function when it executes it and then basically executes "state.counter" to retrieve the part of the state, which we need in this Component. "useSelector" overall gives us that returned value.
+// 1.4 So we get a counter constant: "const counter = useSelector((state) => state.counter)" which is the counter managed by Redux.
+// Then you use "useSelector", Reacr-Redux will automatically set up a subscription to the Redux "store" for "Counter" Component. So your Component will be updated and will receive the latest counter automatically whenever that data changes in the Redux "store". So it's automatically reactive and changes to the Redux "store" will cause "Counter" Component function to be re-executed.
+// SO you always have a latest counter.
+// ~~ USING REDUX DATA IN REACT COMPONENTS ~~
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import store from "./store/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+// ~~ PROVIDINGTHE STORE ~~
+// CAME FROM "store/index.js"
+// STEP 1:
+// This is index.js file where rendered the entire app. Highest level we can go, in our React application, to the top of our Component tree, where we render this root Component.
+// 1.1 Here we can import "import { Provider } from "react-redux""
+// 1.2 Now we wrap all our Component with Provider. "<Provider><App /></Provider>" We wrap entire </ App> not separate child Components.
+// React Redux doesn't know that data file holds our store. Let's import our "store".
+// 1.3 "import store from "./store/index"".
+// 1.4 On "<Provider>", which we import from React Redux we have a store prop, which we have to set. And this one's a value, which is our Redux "store"." <Provider store={store}>"
+// "{store}" - we setting this as a value, for the store prop on this "<Provider>" Component.
+// Now this "store" is provided, but that doesn't change anything at the moment. But now our Component in this App, can tap into that store, can get data out of the "store". They can set up a subscription to that data. They also can dispatch actions.
+// ~~ PROVIDINGTHE STORE ~~

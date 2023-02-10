11d04049acc702065365ef210aacb5a91f35e248
Dmitriy Angve <kirirch129@gmail.com>
2023-02-10 14:42:17 +0300

    Logout and Login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import { Fragment } from "react";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </Fragment>
   );
@@ -26,3 +31,17 @@ export default App;
 // GO TO UserProfile.js --->>>
 
 // WORKING WITH MULTIPLE SLICES
+
+// READING AND DISPATCHING FROM A NEW SLICE
+
+// STEP 1:
+// What we do: render "<Auth />" Component if we're not authenticated, and render <UserProfile /> if we are authenticated.
+// 1.1 "import UserProfile from "./components/UserProfile"".
+// And then we need to tap into our store and read a value from there. And we do that with "useSelector"
+// 1.2 "import { useSelector } from "react-redux";"
+// 1.3 In "App" Component function we therefore call "useSelector()", pass a function to it, function which always receive "state" as an argument, that's passed by Reac-Redux фand return "state.auth" - auth because I picked "auth" as an identifier from "./store/index.js" "const store = configureStore({reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },);" and then in "authSlice" I have this "isAuthenticated" property. "useSelector((state) => state.auth.isAuthenticated);"
+// 1.4 Store it in "isAuth" value.
+// 1.5 Add conditions for render to "{!isAuth && <Auth />}" and if wanna truthy: "{isAuth && <UserProfile />}"
+// NOW we also go some conditional content to render in the Header
+// GO TO Header.js --->>>
+// READING AND DISPATCHING FROM A NEW SLICE
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,17 +1,27 @@
-import classes from './Auth.module.css';
+import classes from "./Auth.module.css";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/index";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const loginHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(authActions.login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
           </div>
           <button>Login</button>
         </form>
@@ -21,3 +31,14 @@ const Auth = () => {
 };
 
 export default Auth;
+
+// READING AND DISPATCHING FROM A NEW SLICE
+
+// STEP 2:
+// 2.1 Add to <form> "onSubmit" listener and add a new function in the Auth Component "const loginHandler =(event)=>{}" where we get default "event" and use "event.preventDefaul()" to make sure Browser doesn't send a HTTP request
+// 2.2 Wired up listener and function
+// 2.3 After this I wanna dispatch that Login action. We wanna import "authAction" and use "dispatch"/// "import { useDispatch } from "react-redux";" /// "import { authActions } from "../store/index";"\
+// 2.4 In "Auth" Component add "const dispatch = useDispatch();"
+// 2.5 And in the "loginHandler" we use this "dispatch(authActions.login());" We execute ".login()" because this is action creator returning the actual action object which should be dispatched.
+// GO TO Header.js --- >>>
+// READING AND DISPATCHING FROM A NEW SLICE
